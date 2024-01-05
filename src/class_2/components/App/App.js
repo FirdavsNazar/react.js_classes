f2f20781affb78afef35683d99d37cc5c150cc53
Firdavs Nazarov <fredrickuzb55@gmail.com>
2024-01-05 09:43:57 +0900

    reatJs.simpleApp

--- src/class_2/components/App/App.js
+++ src/class_2/components/App/App.js
@@ -4,6 +4,7 @@ import AppInfo from "../App-info/App-info"
 import AppFilter from "../App-filter/App-filter"
 import SearchPanel from "../Search-panel/Search-panel"
 import MovieList from '../Movie-list/movie-list'
+import MoviesAddForm from '../Movies-add-form/movies-add-form'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <AppFilter/>
         </div>
         <MovieList/>
+        <MoviesAddForm/>
       </div>
       
     </div>
