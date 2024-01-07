93991d6ca3f5de56e071b0990b7781b0209f7b03
Firdavs Nazarov <fredrickuzb55@gmail.com>
2024-01-07 19:32:58 +0900

    props.react

--- src/class_3 Props/App.jsx
+++ src/class_3 Props/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const User = (props) =>{
+const User = ({firstName, lastName, link}) =>{
  return (
     <div>
-        <h1>Hello everyone my name is - {props.firstName}, and surname is - {props.lastName}</h1>
-        <p>You can check my portfolio by click this link <a href={props.link} target='_blank'> My Portfolio</a></p>
+        <h1>Hello everyone my name is - {firstName.name}, and surname is - {lastName}, age {firstName.age}</h1>
+        <p>You can check my portfolio by click this link <a href={link} target='_blank'> My Portfolio</a></p>
     </div>
  )
 }
@@ -13,8 +13,8 @@ const User = (props) =>{
 const App = () => {
   return (
     <div>
-        <User firstName = 'Firdavs'  lastName= "Nazarov"  link= "mychanel.com"/>
-        <User firstName = 'Abdulloh'  lastName= "MuhammadAliyev"  link= "https://cyber.gachon.ac.kr/"/>
+        <User firstName = {{name: "Firdavsbek", age: 23}}  lastName= "Nazarov"  link= "mychanel.com"/>
+        <User firstName = {{name: "Abdulloh", age: 25}}  lastName= "MuhammadAliyev"  link= "https://cyber.gachon.ac.kr/"/>
     </div>
   )
 }
