ae61efdc9de7475cc3e3ea3a5bf24beec4497aae
Firdavs Nazarov <fredrickuzb55@gmail.com>
2024-01-07 19:26:03 +0900

    props.js

---
+++ src/class_3 Props/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const User = (props) =>{
+ return (
+    <div>
+        <h1>Hello everyone my name is - {props.firstName}, and surname is - {props.lastName}</h1>
+        <p>You can check my portfolio by click this link <a href={props.link} target='_blank'> My Portfolio</a></p>
+    </div>
+ )
+}
+
+const App = () => {
+  return (
+    <div>
+        <User firstName = 'Firdavs'  lastName= "Nazarov"  link= "mychanel.com"/>
+        <User firstName = 'Abdulloh'  lastName= "MuhammadAliyev"  link= "https://cyber.gachon.ac.kr/"/>
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css' 
-import App from './class_2/components/App/App';
+import App from './class_3 Props/App';
+// import App from './class_2/components/App/App';
+
 
 
 
