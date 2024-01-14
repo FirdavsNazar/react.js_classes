1276a7b05c9f1b9a0466371db6d5a58a633f7d30
Firdavs Nazarov <fredrickuzb55@gmail.com>
2024-01-14 22:47:41 +0900

    Inputs.react

--- src/class_2/components/Movies-add-form/movies-add-form.js
+++ src/class_2/components/Movies-add-form/movies-add-form.js
@@ -5,14 +5,29 @@ import './movies-add-form.css'
 // Class Components/////////////////////////////////////////////////////////////////////////////////
 
 class MoviesAddForm extends Component {
+constructor(props){
+  super(props)
+  this.state ={
+    name: '',
+    views: ''
+  }
+
+}
+
+changeName = e =>{
+  this.setState({[e.target.name]: e.target.value})
+  // console.log(e);
+}
+
   render(){
+    const {name, views} = this.state
     return (
       <div className='movies-add-form'>
           <h3>Yangi kino qo'shish</h3>
   
           <form className='add-form d-flex'>
-              <input type="text" className='form-control new-post-label' placeholder='Qanday kino?' />
-              <input type="Number" className="form-control new-post-label' placeholder='Nechi marotaba ko'rilgan?" />
+              <input onChange={this.changeName} name='name' value={name} type="text" className='form-control new-post-label' placeholder='Qanday kino?' />
+              <input onChange={this.changeName} name='views' value={views} type="Number" className="form-control new-post-label' placeholder='Nechi marotaba ko'rilgan?" />
               <button type='submit' className='btn btn-outline-dark'> Qo'shish</button>
           </form>
       </div>
