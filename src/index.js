5e12220013f8e34d236c409cbb86e73acfb9c600
Firdavs Nazarov <fredrickuzb55@gmail.com>
2024-02-11 09:42:42 +0900

    React.sammi

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css' 
 // import App from './class_3 Props/App';
-// import App from './class_4/App';
-import App from './class_2/components/App/App';
+import App from './class_4/App';
+// import App from './class_2/components/App/App';
 
 
 
