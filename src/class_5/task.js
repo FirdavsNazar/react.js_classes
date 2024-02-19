6885c29a2c97dee73c5f0bb45c2e3c2a4ee0559d
Firdavs Nazarov <fredrickuzb55@gmail.com>
2024-02-19 23:23:31 +0900

    js.tasks

---
+++ src/class_5/task.js
@@ -0,0 +1,15 @@
+
+
+// function showCount(count){
+//     return count
+// }
+
+// let abc = showCount(null) == showCount();
+
+// console.log(abc);
+
+let a = 1;
+let b = 2;
+let c = 3- (a= b+1);
+
+console.log(c);
